50afed86bb7301f124a71895b79e697b8b7ac3d8
Nduvho.Mukwevho <divinemukwevho@gmail.com>
2022-04-29 10:09:55 +0200

    New updates on the project and an update in java projects

--- src/main/java/sportsData.java
+++ src/main/java/sportsData.java
@@ -24,7 +24,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.http.HttpRequest;
+
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Scanner;
